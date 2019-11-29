ca015f817ba470535b457dba635be94c99de74d8
pierrepmourier <pmourier@seas.upenn.edu>
2019-11-28 22:07:11 -0500

    adding skill id

--- src/com/amazon/ask/airplanefacts/AirplaneFactsStreamHandler.java
+++ src/com/amazon/ask/airplanefacts/AirplaneFactsStreamHandler.java
@@ -17,7 +17,7 @@ public class AirplaneFactsStreamHandler extends SkillStreamHandler {
                         new SessionEndedRequestHandler(),
                         new FallBackIntentHandler())
                 // Add your skill id below and uncomment to enable skill ID verification
-                // .withSkillId("")
+                .withSkillId("amzn1.ask.skill.6f264775-f5f9-4c66-8b0f-ccd1682733f8")
                 .build();
     }
 
