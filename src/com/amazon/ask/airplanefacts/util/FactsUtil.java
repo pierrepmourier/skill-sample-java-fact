62dba61dbd5c0cf19a99b9e1e0a5e6254eb9b5f1
pierrepmourier <pmourier@seas.upenn.edu>
2019-12-01 18:02:00 -0500

    syncing versions

--- src/com/amazon/ask/airplanefacts/util/FactsUtil.java
+++ src/com/amazon/ask/airplanefacts/util/FactsUtil.java
@@ -10,20 +10,24 @@ public class FactsUtil {
     public static Map getFactMap() {
         Map<String, String> map = new HashMap<>();
         map.put("MostGoals", "The team with the most goals was Belgium with 16.");
-        map.put("GameWithMostGoals", "The game with most goals was X versu Y, with Z goals.");
-        map.put("BestDefense", "The team with the best defense was X with an average of Y goals against per game.");
-        map.put("MostWoodWork", "The team that hit the most posts was X with Y hits.");
-        map.put("MostViolent", "The team with the most red cards was X with Y red cards.");
+        map.put("GameWithMostGoals", "The games with the most goals were: Belgium versus Tunisia."
+        		+ "England versus Panama. and France versus Argentina. They all had 7 goals!");
+        map.put("BestDefense", "The team with the best defense was Denmark with 0.5 goals "
+        		+ "conceded per game.");
+        map.put("MostWoodWork", "The team with the most shots hitting woodwork was: Croatia. "
+        		+ "They had 4 shots that hit woodwork!");
+        map.put("MostViolent", "The teams with the most red cards were: Colombia. and Switzerland. "
+        		+ "They all had 1 red card.");
         return map;
     }
 
     public static Map getImageMap() {
         Map<String, String> map = new HashMap<>();
-        map.put("MostGoals", "https://s3.amazonaws.com/ask-samples-resources/images/airbus.jpg");
-        map.put("GameWithMostGoals", "https://s3.amazonaws.com/ask-samples-resources/images/airplane-1.jpg");
-        map.put("BestDefense", "https://s3.amazonaws.com/ask-samples-resources/images/airplane-2.jpg");
-        map.put("MostWoodWork", "https://s3.amazonaws.com/ask-samples-resources/images/airplane-3.jpg");
-        map.put("MostViolent", "https://s3.amazonaws.com/ask-samples-resources/images/airplane-4.jpg");
+        map.put("MostGoals", "https://cit591-public.s3.amazonaws.com/belgium.jpg");
+        map.put("GameWithMostGoals", "https://cit591-public.s3.amazonaws.com/england.jpg");
+        map.put("BestDefense", "https://cit591-public.s3.amazonaws.com/denmark.jpg");
+        map.put("MostWoodWork", "https://cit591-public.s3.amazonaws.com/croatia.jpg");
+        map.put("MostViolent", "https://cit591-public.s3.amazonaws.com/switzerland.jpg");
         return map;
     }
 
